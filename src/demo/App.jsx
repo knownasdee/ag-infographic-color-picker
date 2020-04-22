36175a879feb73c1094f2ccea8d45da1cc0d1e11
Dada Delimustafic <dadadelimustafic@gmail.com>
2020-04-22 20:06:42 +0200

    added basic color component with fixed color set

---
+++ src/demo/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ColorPicker from '../lib';
+
+const App = () => (
+  <div>
+    <ColorPicker />
+  </div>
+);
+
+export default App;
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,3 @@
-import Example from './components/Example';
-import SecondExample from './components/SecondExample';
+import ColorPicker from './components/ColorPicker';
 
-export default Example;
-
-export {
-  SecondExample,
-};
+export default ColorPicker;
