1a0acefa1a2a68707cb60fe34aafe7c9b55e5ab8
Dada Delimustafic <dadadelimustafic@gmail.com>
2020-05-15 16:18:33 +0200

    using nwb setup

--- src/lib/components/ColorPicker.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ColorPicker from './ColorPicker';
-
-it('ColorPicker renders without crashing', () => {
-    const div = document.createElement('div');
-
-    ReactDOM.render(<ColorPicker />, div);
-});
