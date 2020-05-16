d496e6c66f781c5d47059df81a8789e0e5a0f0cd
Dada Delimustafic <dadadelimustafic@gmail.com>
2020-05-16 23:39:26 +0200

    creates ColorPicker component with styles

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import ColorPickerComponent from '../../src'
 
 export default class Demo extends Component {
   render() {
     return <div>
       <h1>ag-infographic-color-picker Demo</h1>
-      <Example/>
+      <ColorPickerComponent/>
     </div>
   }
 }
